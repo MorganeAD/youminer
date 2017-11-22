d639f4e804355a53cc9be3221751a71f8363e33a
Alexis Ben Miloud--Josselin <alexis.ben-miloud-josselin@etu.univ-amu.fr>
2017-11-22 10:46:54 +0100

    Fix JSON parsing

--- get_links.py
+++ get_links.py
@@ -4,7 +4,7 @@ import json
 json_data = open('data.json')
 data = json.load(json_data)
 json_data.close()
-for i in range(len(data) - 1):
+for i in range(len(data['items']) - 1):
 	url = "https://youtu.be/" + data['items'][i]['id']['videoId']
 	title = data['items'][i]['snippet']['title']
 	channel = data['items'][i]['snippet']['channelTitle']
