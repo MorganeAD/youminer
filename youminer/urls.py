240aabea176c133dd65183355522af2b01182d16
Morgane Alonso <morgane.alonso@etu.univ-amu.fr>
2017-12-21 11:09:44 +0100

    read video from vlc

--- youminer/urls.py
+++ youminer/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^videos/(?P<cat>[\w ]+)/$', views.video_list, name='video_list'),
+    url(r'^video/(?P<vId>[\w ]+)/$', views.video_show, name='video_show'),
     url(r'^authors/(?P<cat>[\w ]+)/$', views.author_list, name='author_list'),
 ]
--- youminer/views.py
+++ youminer/views.py
@@ -3,9 +3,30 @@ from .models import Video
 from .models import Category
 from .models import Author
 
+import os
+
+def video_show(request, vId):
+    video = Video.objects.get(videoId=vId, author__isnull=False)
+    f = open('youminer/port','r')
+    port = int(f.read())
+    f = open('youminer/port','w')
+    if port > 60010:
+        f.write('60000')
+    else:
+        f.write(str(port+1))
+    port = 8080
+
+    # Run vlc in a fork
+    newpid = os.fork()
+    if newpid == 0:
+        os.system('vlc --intf dummy ' + video.url + ' :sout="#transcode{vcodec=theo,vb=800,scale=0.25,acodec=vorb,ab=128,channels=2,samplerate=44100}:http{mux=ogg,dst=:' + str(port) + '/}" :sout-keep')
+        os._exit(0)
+
+    return render(request, 'youminer/video_show.html', {'video': video, 'port' : port})
+
 def video_list(request, cat):
     videosCategory = Category.objects.get(name__iexact=cat)
-    videos = Video.objects.filter(category=videosCategory.id)
+    videos = Video.objects.filter(category=videosCategory.id, author__isnull=False)
     categories = Category.objects.all()
     return render(request, 'youminer/video_list.html', {'currentCat':cat, 'videos': videos, 'categories' : categories})
 
