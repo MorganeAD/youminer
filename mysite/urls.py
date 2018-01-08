3522197c192546b0cffd2696a4a91b86fa3c71c7
Morgane Alonso <morgane.alonso@etu.univ-amu.fr>
2018-01-08 15:32:29 +0100

    add prettiness

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'', include('youminer.urls')),
 ]
--- youminer/views.py
+++ youminer/views.py
@@ -19,19 +19,20 @@ def video_show(request, vId):
     # Run vlc in a fork
     newpid = os.fork()
     if newpid == 0:
-        os.system('vlc --intf dummy ' + video.url + ' :sout="#transcode{vcodec=theo,vb=800,scale=0.25,acodec=vorb,ab=128,channels=2,samplerate=44100}:http{mux=ogg,dst=:' + str(port) + '/}" :sout-keep')
+        os.system('vlc --intf dummy --play-and-exit ' + video.url + ' :sout="#transcode{vcodec=theo,vb=800,scale=0.25,acodec=vorb,ab=128,channels=2,samplerate=44100}:http{mux=ogg,dst=:' + str(port) + '/}" :sout-keep')
         os._exit(0)
 
-    return render(request, 'youminer/video_show.html', {'video': video, 'port' : port})
+    categories = Category.objects.all()
+    return render(request, 'youminer/video_show.html', {'video': video, 'port' : port, 'categories' : categories})
 
 def video_list(request, cat):
     videosCategory = Category.objects.get(name__iexact=cat)
     videos = Video.objects.filter(category=videosCategory.id, author__isnull=False)
     categories = Category.objects.all()
-    return render(request, 'youminer/video_list.html', {'currentCat':cat, 'videos': videos, 'categories' : categories})
+    return render(request, 'youminer/video_list.html', {'videos': videos, 'categories' : categories})
 
 def author_list(request, cat):
     authorsCategory = Category.objects.get(name__iexact=cat)
     authors = Author.objects.filter(category=authorsCategory.id)
     categories = Category.objects.all()
-    return render(request, 'youminer/author_list.html', {'currentCat':cat, 'authors': authors, 'categories' : categories})
+    return render(request, 'youminer/author_list.html', {'authors': authors, 'categories' : categories})
