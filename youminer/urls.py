70bb05ee9388ee4da7279ac4bcb883a171fd4b1f
Alexis Ben Miloud--Josselin <alexis.bmj@protonmail.com>
2018-02-04 13:50:47 +0100

    Lot of stuff about questions

--- youminer/urls.py
+++ youminer/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^video/(?P<vId>[\w ]+)/$', views.video_show, name='video_show'),
     url(r'^authors/(?P<cat>[\w ]+)/$', views.author_list, name='author_list'),
     url(r'^questions/$', views.questions, name='question'),
+    url(r'^questions_finished/$', views.questions, name='question'),
+    url(r'^questions_cannot/$', views.questions, name='question'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name='youminer/login.html'), name='login'),
     url(r'^logout/$', views.logout_page, name='logout_page'),
     url(r'^registration/$', views.add_user, name='add_user'),
